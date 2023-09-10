8af255e582e8d7b37192aee3aff1787a0e6ee46e
Nicolas <nicolasgalvez1985@gmail.com>
2023-09-10 20:40:57 -0300

    Se cambia el modo de agregar roles mediate checkbox

--- js/create.js
+++ js/create.js
@@ -1,71 +1,29 @@
-
-// Variables para el manejo de roles dinámicos
-const rolesList = document.getElementById('roles-list');
-const agregarRolButton = document.getElementById('agregar-rol');
-const formulario = document.querySelector('form');
-
-// Función para agregar un campo de entrada de rol
-function agregarCampoRol() {
-    const nuevoCampoRol = document.createElement('li');
-    const nuevoCampoRolHTML = `
-    <div class="form-group row align-items-center">
-        <div class="col-6 mx-0">
-            <select name="rol[]" class="form-control form-control-lg" style="max-width: 100%;">
-                <option value="" disabled selected>Seleccione un Rol</option>
-                <option value="0">Administrador</option>
-                <option value="1">Regente</option>
-                <option value="2">Profesor</option>
-                <option value="3">Alumno</option>
-                <option value="4">Bedel</option>
-                <option value="5">Secretario</option>
-            </select>
-        </div>
-        <div class="col-7 mx-0 pt-1">
-            <button type="button" class="btn btn-danger eliminar-rol">Eliminar Rol</button>
-        </div>
-    </div>
-    `;
-    nuevoCampoRol.innerHTML = nuevoCampoRolHTML;
-    rolesList.appendChild(nuevoCampoRol);
-
-    // Agregar evento de eliminación para el botón
-    const eliminarRolButton = nuevoCampoRol.querySelector('.eliminar-rol');
-    eliminarRolButton.addEventListener('click', function() {
-        rolesList.removeChild(nuevoCampoRol);
+document.addEventListener("DOMContentLoaded", function () {
+    const adminCheckbox = document.querySelector('input[name="rol[]"][value="0"]');
+    const otherCheckboxes = document.querySelectorAll('input[name="rol[]"]:not([value="0"])');
+  
+    adminCheckbox.addEventListener("change", function () {
+      if (this.checked) {
+        // Si se selecciona el checkbox de Administrador, deselecciona y deshabilita los otros checkboxes
+        otherCheckboxes.forEach(function (checkbox) {
+          checkbox.checked = false;
+          checkbox.disabled = true;
+        });
+      } else {
+        // Si se deselecciona el checkbox de Administrador, habilita los otros checkboxes
+        otherCheckboxes.forEach(function (checkbox) {
+          checkbox.disabled = false;
+        });
+      }
     });
-
-    // Evento para controlar selecciones duplicadas
-    const selectElement = nuevoCampoRol.querySelector('select');
-    selectElement.addEventListener('change', function() {
-        const selectedValue = selectElement.value;
-        if (selectedValue !== '' && rolesSeleccionados.has(selectedValue)) {
-            alert('Este rol ya ha sido seleccionado.');
-            selectElement.value = ''; // Reiniciar la selección
-        } else {
-            rolesSeleccionados.add(selectedValue);
-        }
-    });
-}
-
-// Función para validar la selección de roles antes de enviar el formulario
-function validarRoles(event) {
-    const selectElements = document.querySelectorAll('select[name^="rol"]');
-    let alMenosUnRolSeleccionado = false;
-
-    selectElements.forEach((selectElement) => {
-        if (selectElement.value !== '') {
-            alMenosUnRolSeleccionado = true;
-        }
-    });
-
-    if (!alMenosUnRolSeleccionado) {
-        alert('Debe seleccionar al menos un rol.');
-        event.preventDefault(); // Evitar el envío del formulario
+  
+    // Verificar si el checkbox de Administrador está seleccionado inicialmente
+    if (adminCheckbox.checked) {
+      // Si está seleccionado, deshabilita y deselecciona los otros checkboxes
+      otherCheckboxes.forEach(function (checkbox) {
+        checkbox.checked = false;
+        checkbox.disabled = true;
+      });
     }
-}
-
-// Agregar el evento de validación al formulario
-formulario.addEventListener('submit', validarRoles);
-
-// Evento para agregar un campo de entrada de rol al hacer clic en el botón
-agregarRolButton.addEventListener('click', agregarCampoRol);
+  });
+  
